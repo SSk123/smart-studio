13218d54cbad90208d07f4bf2245771ac6fbe588
Srishti <srishsensation@gmail.com>
2024-01-30 14:59:27 +0530

    landing page

--- studio-frontend/src/App.js
+++ studio-frontend/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+import * as React from 'react';
+import './App.css';
+import {Skeleton} from '@mui/material';
+import Navigation from './Navigation';
+import AddIcon from '@mui/icons-material/Add';
+import CameraSharpIcon from '@mui/icons-material/CameraSharp';
+
+export default function App() {
+  const [page, setPage] = React.useState(0);
+
+  return <div className='App'>
+      <header className='App-header'>
+        <CameraSharpIcon class="app-logo"/>
+        <h3>Welcome user,</h3>
+        <div className='app-link'>Click <AddIcon className='add-icon' onClick={(event, newValue) => {
+            setPage(newValue);
+          }}/> to start a project </div>
       </header>
     </div>
-  );
 }
-
-export default App;
